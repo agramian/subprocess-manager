05eeb16daa963469f183bace2e62d9076ecdb7b6
Abtin Gramian <abtin.gramian@gmail.com>
2015-09-24 16:28:03 -0700

    created setup.py file for pip installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='subprocess_manager',
+      version='1.0',
+      description='Python Subprocess Manager',
+      author='Abtin Gramian',
+      author_email='abtin.gramian@gmail.com',
+      url='https://github.com/agramian/subprocess-manager',
+      packages=['subprocess_manager'],
+     )
